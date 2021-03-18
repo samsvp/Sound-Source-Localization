1ee257d8920a9367d4a4c99690cf1bff19f73423
Samuel Simplicio <samuelsimplicio@poli.ufrj.br>
2021-03-18 15:37:19 -0300

    Fixed sound generator

--- simulator_hydrophone_audio.py
+++ simulator_hydrophone_audio.py
@@ -1,78 +1,78 @@
-#!/usr/bin/env python3
 # %%
 import numpy as np
 import beamforming as bf
-import visual_beamforming as vbf
-
+from scipy.interpolate import CubicSpline
 
 class AudioGenerator:
 
     def __init__(self, coord, fs, num_samples):
         self.fs = fs
-        self.num_samples = num_samples  # Number of samples to read
-
-        sound_speed = 1491.24  # m/s
-
+        self.num_samples = num_samples # Number of samples to read
+        
+        sound_speed  = 1491.24 # m/s
+        
         # Delay matrix builder
-        self.phi = np.deg2rad(np.arange(0, 181)).reshape(1, 181)
-        self.theta = np.deg2rad(np.arange(0, 181)).reshape(181, 1)
+        self.phi = np.deg2rad(np.arange(0,181)).reshape(1,181)
+        self.theta = np.deg2rad(np.arange(0, 181)).reshape(181,1)
 
-        spherical_coords = np.concatenate(
-            (
-                [-np.cos(self.theta)*np.sin(self.phi)],
-                [np.sin(self.theta)*np.sin(self.phi)],
-                [np.ones(self.theta.shape)
-                 * np.cos(self.phi)]
-            )
-        )
+        spherical_coords = np.concatenate( 
+                                    (
+                                        [-np.cos(self.theta)*np.sin(self.phi)],
+                                        [np.sin(self.theta)*np.sin(self.phi)],
+                                        [np.ones(self.theta.shape)*np.cos(self.phi)]
+                                    )
+                                )
 
         delays = np.array(
-            [np.dot(coord, spherical_coords[..., i])
-             for i in range(spherical_coords.shape[-1])]
-        )/sound_speed  # Shape (phi.shape, hydrophone_number, theta.shape)
+                        [np.dot(coord, spherical_coords[...,i]) 
+                        for i in range(spherical_coords.shape[-1])]
+                    )/sound_speed  # Shape (phi.shape, hydrophone_number, theta.shape)	
+
+        time_delays = np.swapaxes(
+				(fs * delays).T, 1, 2
+			) # Shape (phi.shape, theta.shape, hydrophone_number)
+
+        self.time_delays = time_delays + np.max(np.abs(time_delays))
+        self.n_max = int(np.max(np.abs(self.time_delays)))
 
         # Frequency delays
-        # Shape (hydrophone_number, theta.shape, phi.shape)
-        deltas = np.moveaxis(delays, 0, -1)
-
-        # Shape (num_samples // 2, hydrophone_number, theta.shape, phi.shape)
-        self.freq_delays = np.array([np.exp(2j * np.pi * fs * deltas * k / num_samples)
-                                     for k in range(num_samples // 2)])
-
-    def create_sine_wave(self, f=5000) -> np.ndarray:
+        # Shape (hydrophone_number, theta.shape, phi.shape)	
+        deltas = np.moveaxis(delays, 0, -1) 
+        self.deltas = deltas
+        # Shape (num_samples // 2, hydrophone_number, theta.shape, phi.shape)	
+        self.freq_delays = np.array([np.exp(-2j * np.pi * fs * deltas * k / num_samples) 
+                            for k in range(num_samples // 2)]) 	
+    
+    def create_sine_wave(self, f=1000) -> np.ndarray:
         """
         Creates a sine wave with 'num_samples' points with the given frequency
         """
-        t = self.num_samples / self.fs  # duration of signal (in seconds)
-        samples = np.linspace(0, t, self.num_samples, endpoint=False)
+        t = self.num_samples / self.fs # duration of signal (in seconds)
+        samples = np.linspace(0, t, self.num_samples//2, endpoint=False)
         signal = np.sin(2 * np.pi * f * samples)
         return signal
+
+    def interpolate(self, y:np.ndarray) -> CubicSpline:
+        x = np.arange(len(y))
+        cs = CubicSpline(x, y)
+        return cs
 
     def shift_signal(self, sine_wave: np.ndarray, azimuth: int, elevation: int) -> np.ndarray:
         """
         Shifts the signal given the azimuth and elevation angles
         """
-        signal = np.fft.fft(sine_wave, axis=0)[:self.num_samples // 2, :]
-        shifted_signal_f = signal * self.freq_delays[:, :, azimuth, elevation]
-        shifted_signal = np.fft.ifft(shifted_signal_f, axis=0).real
-        return shifted_signal
+        x = np.arange(len(sine_wave))
+        cs = self.interpolate(sine_wave)
+        shifted_signal = [cs(x - delay) for delay in self.time_delays[azimuth, elevation, :]]
+        return np.array(shifted_signal)
 
-    def create_signals(self, azimuth: int, elevation: int, f=5000) -> np.ndarray:
+    def create_signals(self, azimuth: int, elevation: int, f=1000) -> np.ndarray:
         sine_wave = self.create_sine_wave(f)
-        sine_waves = np.array([sine_wave, sine_wave, sine_wave, sine_wave]).T
+        sine_waves = np.array([sine_wave for i in range(self.freq_delays.shape[1])]).T
         shifted_signal = self.shift_signal(sine_waves, azimuth, elevation)
-        shifted_signal = self.create_zeroes(shifted_signal)
-        return shifted_signal, self.fs
-
-    def create_zeroes(self, shifted_signal: np.ndarray) -> np.ndarray:
-
-        for i in range(self.num_samples//4):
-            for j in range(len(shifted_signal[i])):
-                shifted_signal[i, j] = 0
-
         return shifted_signal
 
-
+#%%
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
 
@@ -80,31 +80,22 @@ if __name__ == "__main__":
     distance_y = (18.37e-3)/2
 
     coord = np.array(([-distance_x, -8.41e-3, -distance_y],
-                      [distance_x, 0, -distance_y],
-                      [distance_x, -8.64e-3, distance_y],
-                      [-distance_x, -0.07e-3, distance_y]
-                      ))
+                    [distance_x, 0, -distance_y],
+                    [distance_x, -8.64e-3, distance_y],
+                    [-distance_x, -0.07e-3, distance_y]
+                ))
 
-    a = AudioGenerator(coord, 192000, 256)
-    shifted_signal, fs = a.create_signals(120, 130)
-    plt.plot(shifted_signal)
-    plt.show()
+    fs = 192000
+    num_samples = 256
+    b = bf.Bf(coord, fs, num_samples//2)
+    a = AudioGenerator(coord, fs, num_samples)
 
-    b = bf.Bf(coord, fs, 128)
-
-    angle = b.fast_aoa(shifted_signal)
-    print("fast aoa angle:", angle)
-
-    angle = b.fast_faoa(shifted_signal)
-    print("freq fast aoa angles:", angle)
-
-    angle = b.aoa(shifted_signal, b.fdsb, batch_size=8)
-    print("normal freq aoa angles:", angle)
-
-    angle = b.aoa(shifted_signal, b.dsb)
-    print("normal time aoa angles:", angle)
-
-    squared_conv = b.dsb(shifted_signal)
-    vbf.plot_squared_conv(squared_conv, show=True)
-
-# %%
+    errs = []
+    for i in range(30,150):
+        az = i
+        el = i
+        sine_wave = a.create_sine_wave(f=5000)
+        y = a.shift_signal(sine_wave[:128], az, el)
+        angle = b.fast_faoa(y.T)
+        errs.append((az-angle[0], el-angle[1]))
+    plt.plot(errs)
