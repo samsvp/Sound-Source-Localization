fbcc90533f09bd988430d410cd5434aed912e3f9
Samuel Simplicio <samuelsimplicio@poli.ufrj.br>
2021-03-21 22:20:00 -0300

    Added neural networks

--- filter.py
+++ filter.py
@@ -63,7 +63,7 @@ b = bf.Bf(coord, fs, num_samples // 2)
 audio_generator = create_audio_generator(coord, fs, num_samples)
 
 angles_range = (0, 180)
-training_size = 5000
+training_size = 10000
 _X, y = generate_training_set(training_size, audio_generator, f=5000, add_noise=False, angles_range=angles_range)
 
 validation_size = training_size // 10
@@ -148,13 +148,6 @@ for regressor in regressors:
     plt.plot(e, "o-")
     plt.show()
 
-e = []
-for i in range(len(_X_val)):
-    a = b.fast_aoa(_X_val[i])[0]
-    e.append(a - az_val[i])
-plt.plot(e, "-")
-plt.show()
-
 # %%
 # train classifiers
 for classifier in classifiers:
@@ -174,6 +167,7 @@ for classifier in classifiers:
     plt.plot(e, "o-")
     plt.show()
 
+#%%
 e = []
 for i in range(len(_X_val)):
     a = b.fast_aoa(_X_val[i])[0]
@@ -181,3 +175,99 @@ for i in range(len(_X_val)):
 plt.plot(e, "-")
 plt.show()
 # %%
+predictors = [regressors["KNeighborsRegressor"], regressors["SVR"],
+                regressors["DecisionTreeRegressor"], regressors["RandomForestRegressor"]]
+
+for i in range(len(predictors)):
+    predictors[i].fit(X, az)
+
+outputs = [[pred.predict(x.reshape(-1, 3))[0] for pred in predictors] for x in X]
+for i in range(len(outputs)):
+    outputs[i].append(b.fast_aoa(_X[i])[0])
+
+
+# %%
+import tensorflow as tf
+
+from tensorflow import keras
+from tensorflow.keras import layers
+
+def build_model():
+    model = keras.Sequential([
+        layers.experimental.preprocessing.Rescaling(1/np.max(X)),
+        layers.Dense(128, activation='relu', input_shape=[3]),
+        layers.Dense(64, activation='relu'),
+        layers.Dense(64, activation='relu'),
+        layers.Dense(181)
+    ])
+
+    model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                optimizer="adam",
+                metrics=['accuracy'])
+    return model
+
+model = build_model()
+
+for i in range(10):
+    EPOCHS = 1000
+
+    history = model.fit(np.array(X), np.array(az),
+    epochs=EPOCHS, validation_split=0.2, verbose=1)
+
+    eb = []
+    e = []
+    for k in data:
+        _x = [np.array([moving_average(d, 10) for d in np.array(dt).T]) for dt in data[k]]
+        ps = [model.predict(get_phase_shift(np.array(x.T)).reshape(-1, 3)) for x in _x]
+        print(k, np.argmax(ps[0]), gab_030719[k][0], np.argmax(ps[0]) - gab_030719[k][0])
+        e.append(np.abs(np.argmax(ps[0]) - gab_030719[k][0]))
+        eb.append(np.abs(b.fast_faoa(np.array(data[k][0]))[0] - gab_030719[k][0]))
+    plt.plot(e, "o-")
+    plt.plot(eb, "o-r")
+    plt.title(i)
+    plt.savefig(f"{i}_a.png")
+    plt.show()
+
+# %%
+import tensorflow as tf
+
+from tensorflow import keras
+from tensorflow.keras import layers
+
+def build_model_b():
+    model = keras.Sequential([
+        layers.experimental.preprocessing.Rescaling(1./181),
+        layers.Dense(128, activation='relu', input_shape=[len(outputs[0])]),
+        layers.Dense(64, activation='relu'),
+        layers.Dense(181)
+    ])
+    model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                optimizer="adam",
+                metrics=['accuracy'])
+    return model
+
+model = build_model_b()
+
+for i in range(10):
+    EPOCHS = 1000
+    history = model.fit(np.array(outputs), np.array(az),
+    epochs=EPOCHS, validation_split=0.2, verbose=1)
+    e = []
+    eb = []
+    for k in data:
+        _x = [np.array([moving_average(d, 10) for d in np.array(dt).T]) for dt in data[k]]
+        _ps = [[
+            pred.predict(get_phase_shift(np.array(x.T)).reshape(-1, 3))[0] 
+        for pred in predictors] for x in _x]
+        for j in range(len(_ps)):
+            _ps[j].append(b.fast_faoa(np.array(data[k][j]))[0])
+        ps = model.predict(np.array(_ps))
+        print(k, np.argmax(ps[0]), gab_030719[k][0], np.argmax(ps[0]) - gab_030719[k][0])
+        eb.append(np.abs(b.fast_faoa(np.array(data[k][0]))[0] - gab_030719[k][0]))
+        e.append(np.abs(np.argmax(ps[0]) - gab_030719[k][0]))
+    plt.plot(e, "o-b")
+    plt.plot(eb, "o-r")
+    plt.title(i)
+    plt.savefig(f"{i}_b.png")
+    plt.clf()
+    model.save(f"weights/model_{i}")
